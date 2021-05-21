da28f29346729c7ac76c8431e1d44fcc2eee6a0f
sasilva1998 <sasilva1998@gmail.com>
2021-05-20 22:51:35 -0500

    feat: ota led test

--- app/start.py
+++ app/start.py
@@ -1,1 +1,11 @@
 print("Version 2 installed using USB")
+import time
+from machine import Pin
+
+led = Pin(2, Pin.OUT)
+
+while True:
+    led.value(1)
+    time.sleep(0.5)
+    led.value(0)
+    time.sleep(0.5)
