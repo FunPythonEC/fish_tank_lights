d6a6a672cf67bfe6654c691a643d43e9740c03eb
sasilva1998 <sasilva1998@gmail.com>
2021-05-21 00:14:14 -0500

    bug: turn off leds

--- app/start.py
+++ app/start.py
@@ -3,11 +3,14 @@ import time
 
 import machine, neopixel
 
-leds_number = 20
+leds_number = 30
 pin_led = 26
 
 np = neopixel.NeoPixel(machine.Pin(pin_led), leds_number)
 
-for i in range(4, leds_number - 5):
+for i in range(0, leds_number):
+    np[i] = [0, 0, 0]
+
+for i in range(0, leds_number - 20):
     np[i] = (25, 25, 75)
 np.write()
