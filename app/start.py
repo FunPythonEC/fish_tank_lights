7a37926feb38f84b21c4959bcdacaec315a07204
sasilva1998 <sasilva1998@gmail.com>
2021-05-20 23:31:03 -0500

    feat: white light

--- app/start.py
+++ app/start.py
@@ -3,8 +3,13 @@ import time
 
 import machine, neopixel
 
-np = neopixel.NeoPixel(machine.Pin(26), 29)
+leds_number = 30
+pin_led = 26
 
-for i in range(0, 29):
-    np[i] = (255, 255, 0)
+np = neopixel.NeoPixel(machine.Pin(pin_led), leds_number)
+
+for i in range(
+    leds_number,
+):
+    np[i] = (255, 255, 255)
 np.write()
