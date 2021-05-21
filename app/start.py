e32b11d05b79f1907563c4e84d5c3956c49fa6d4
sasilva1998 <sasilva1998@gmail.com>
2021-05-21 00:05:27 -0500

    feat: less leds

--- app/start.py
+++ app/start.py
@@ -8,8 +8,6 @@ pin_led = 26
 
 np = neopixel.NeoPixel(machine.Pin(pin_led), leds_number)
 
-for i in range(
-    leds_number,
-):
+for i in range(4, leds_number - 5):
     np[i] = (25, 25, 75)
 np.write()
